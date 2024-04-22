6a63d224aeb3284a22b3efcad7cb814c257a083f
samjhopkins9 <samjhopkins9@gmail.com>
2024-04-22 12:29:41 -0400

    Added Chaos EP

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let logo = "Images/REDWAVE.logo.png";
 
-let descwritelink = "https://www.badmouthrecs.com/post/redwave-a-sonic-fusion-of-gritty-rhythmic-and-complex-bass-music-in-descent-ep";
-
 let spotlink = "https://open.spotify.com/artist/3DUGOEQlHtDibMS73uULP8?si=WNgl6pSyS5-OaHhZOzosgg";
 let applink = "https://music.apple.com/us/artist/redwave/1704251598";
 let soundlink = "https://soundcloud.com/whoisredwave";
@@ -13,19 +11,21 @@ let twtrlink = "https://twitter.com/whoisredwave";
 let ttlink = "https://www.tiktok.com/@whoisredwave";
 let ytlink = "https://www.youtube.com/channel/UCdt1Bwh674LaxiD5-D2F98g";
 
-let descent = new Release("Descent EP", descentsc, descentspot, descentappl, descentdl)
-let immortal = new Release("Immortal", immortalsc, immortalspot, immortalappl, immortaldl)
-let motor = new Release("Motor", motorsc, motorspot, motorappl, motordl)
-let lightning = new Release("Lightning", lightningsc, lightningspot, lightningappl, lightningdl)
+let descwritelink = "https://www.badmouthrecs.com/post/redwave-a-sonic-fusion-of-gritty-rhythmic-and-complex-bass-music-in-descent-ep";
 
 document.querySelector("header").innerHTML += `<img src="${logo}" width="20%" height="10%"></img>`;
-
-document.getElementById("links").innerHTML += `<a href=${descwritelink} target="_BLANK"><div class="link">DESCENT EP WRITEUP</div></a>`
 
 document.getElementById("links").innerHTML += `<a href=${soundlink} target="_BLANK"><div class="link"><img src="Images/soundcloud-logo.png" width="30px"></img>Soundcloud</div></a>`;
 document.getElementById("links").innerHTML += `<a href=${spotlink} target="_BLANK"><div class="link"><img src="Images/spotify-logo.png" width="30px"></img>Spotify</div></a>`;
 document.getElementById("links").innerHTML += `<a href=${applink} target="_BLANK"><div class="link"><img src="Images/apple-music-logo.png" width="30px"></img>Apple Music</div></a>`;
 
+let chaos = new Release("Chaos EP", chaossc, chaosspot, chaosappl, chaosdl)
+let descent = new Release("Descent EP", descentsc, descentspot, descentappl, descentdl)
+let immortal = new Release("Immortal", immortalsc, immortalspot, immortalappl, immortaldl)
+let motor = new Release("Motor", motorsc, motorspot, motorappl, motordl)
+let lightning = new Release("Lightning", lightningsc, lightningspot, lightningappl, lightningdl)
+
+document.getElementById("links").innerHTML += `<a href=${descwritelink} target="_BLANK"><div class="link">DESCENT EP WRITEUP</div></a>`
 
 document.getElementById("links").innerHTML += `<a href=${instalink} target="_BLANK"><div class="link"><img src="Images/Logo-Instagram-noir-1.png" width="30px"></img>Instagram</div></a>`;
 document.getElementById("links").innerHTML += `<a href=${ttlink} target="_BLANK"><div class="link"><img src="Images/tiktok-logo.png" width="30px"></img>TikTok</div></a>`;
