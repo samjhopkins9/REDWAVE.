5063f927eb850fc22b63102e852c1dfd9ec5044c
samjhopkins9 <samjhopkins9@gmail.com>
2024-04-22 22:19:20 -0400

    edits

--- songlinks.js
+++ songlinks.js
@@ -1,6 +1,6 @@
-let chaossc = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1805557528&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
-let chaosspot = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/0pMh6T8yvCpnyV7OZEUX3n?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
-let chaosappl = `<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/chaos-ep/1738753652"></iframe>`
+let chaossc = `<iframe width="33%" height="352" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1805557528&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
+let chaosspot = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/0pMh6T8yvCpnyV7OZEUX3n?utm_source=generator" width="33%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+let chaosappl = `<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="352" width="33%" style="max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/chaos-ep/1738753652"></iframe>`
 let chaosdl = ``
 
 let lightningsc = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1605970608&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`;
@@ -18,23 +18,32 @@ let immortalspot = `https://open.spotify.com/track/6EsLeP93n2lbLXAgyTQTrJ?si=539
 let immortalappl = `https://music.apple.com/us/album/immortal/1706802515?i=1706802516`;
 let immortaldl = `https://hypeddit.com/redwave/immortal`;
 
-let descentsc = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1758975567&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
-let descentspot = `https://open.spotify.com/album/3V1EzIVtYlcnb86qIMmmtz?si=qDd2NSBWRIuuufnN0vfyzw`
-let descentappl = `https://music.apple.com/us/album/descent-ep/1725616492`
+let descentsc = `<iframe width="33%" height="352" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1758975567&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
+let descentspot = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/3V1EzIVtYlcnb86qIMmmtz?utm_source=generator" width="33%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+let descentappl = `<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="352" width="33%" style="max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/descent-ep/1725616492"></iframe>`
 let descentdl = `https://hypeddit.com/redwave/planetary`
+let descentwriteup = `https://www.badmouthrecs.com/post/redwave-a-sonic-fusion-of-gritty-rhythmic-and-complex-bass-music-in-descent-ep`
 
 class Release {
     
-    constructor(name, sc, spot, appl, dl){
+    constructor(name, sc, spot, appl, dl, writeup = null){
         
         this.name = name;
         this.spot = spot;
         this.appl = appl;
         this.sc = sc;
         this.dl = dl;
+        if (writeup){
+            this.writeup = writeup;
+        }
+        
         
         let rd = document.createElement("div");
         rd.innerHTML += `<h2>${name}</h2>`;
+        if (writeup){
+            rd.innerHTML += `<a href=${writeup} target="_BLANK" class="songlink">WRITEUP</a><br><br>`;
+        }
+        
         rd.innerHTML += sc;
         
         if (this.spot.length > 100){
@@ -43,9 +52,12 @@ class Release {
             rd.innerHTML += appl;
             
         } else {
-            rd.innerHTML += `<a href=${dl} target="_BLANK" class="songlink">FREE DL</a>   <a href=${spot} target="_BLANK" class="songlink">SPOTIFY</a>  <a href=${appl} target="_BLANK" class="songlink">APPLE MUSIC</a>`;
-            
+            rd.innerHTML += `<br><a href=${spot} target="_BLANK" class="songlink">SPOTIFY</a>  <a href=${appl} target="_BLANK" class="songlink">APPLE MUSIC</a>`;
         }
+        if (dl != ''){
+            rd.innerHTML += `<a href=${dl} target="_BLANK" class="songlink">FREE DL</a>`;
+        }
+        
         
         
         document.getElementById("releases").appendChild(rd);
